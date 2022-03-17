6b2d42285ca8f01ebebd8c575d16c4f9677705fa
Jerónimo Hernández-González <jhernandez.glez@gmail.com>
2022-03-17 18:01:32 +0100

    Add cell to allow nvcc flags

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
 ## NVCC Plugin for Jupyter notebook
 
-### V2 is available
+Provides three magic cells with different capabilities. 
 
-V2 brings support of multiple source and header files.
-
-##### Usage
+### Usage
 
 - Load Extension
 > `%load_ext nvcc_plugin`
 
-- Mark a cell to be treated as cuda cell
+- To compile and run a CUDA cell
+> `%%cu`
+> and add your code below
+
+- To compile with specific flags and run a CUDA cell
+> `%%cuf -f-flag1+-flag2`
+>> WHERE: all the required nvcc flags are concatenated with interleaved character `+` 
+>> (do not leave any blank space nor among flags neither after `-f`)
+
+- Mark a cell to be treated as cuda library
 > `%%cuda --name example.cu --compile false`
 >> NOTE: The cell must contain either code or comments to be run successfully. 
 >> It accepts 2 arguments. `-n` | `--name`  - which is the name of either CUDA source or Header
 >> The name parameter must have extension `.cu` or `.h`
 >> Second argument `-c` | `--compile`; default value is `false`. The argument is a flag to specify
->> if the cell will be compiled and run right away or not. It might be usefull if you're playing in
+>> if the cell will be compiled and run right away or not. It might be useful if you're playing in
 >> the `main` function
-
-- To compile and run all CUDA files you need to run
-```
-%%cuda_run
-# This line just to bypass an exeption and can contain any text
-```
--- v2/v2.py
+++ v2/v2.py
@@ -1,8 +1,11 @@
 import os
 import subprocess
+import tempfile
+import uuid
 
 from IPython.core.magic import Magics, cell_magic, magics_class
 from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
+
 from common import helper
 
 compiler = '/usr/local/cuda/bin/nvcc'
@@ -13,7 +16,6 @@ class NVCCPluginV2(Magics):
 
     def __init__(self, shell):
         super(NVCCPluginV2, self).__init__(shell)
-        self.argparser = helper.get_argparser()
         current_dir = os.getcwd()
         self.output_dir = os.path.join(current_dir, 'src')
         if not os.path.exists(self.output_dir):
@@ -26,10 +28,10 @@ class NVCCPluginV2(Magics):
         print(f'Out bin {self.out}')
 
     @staticmethod
-    def compile(output_dir, file_paths, out):
-        res = subprocess.check_output(
-            [compiler, '-I' + output_dir, file_paths, "-o", out, '-Wno-deprecated-gpu-targets'], stderr=subprocess.STDOUT)
-        helper.print_out(res)
+    def compile(output_dir, file_paths, out, flags=[]):
+        res = subprocess.check_output( [compiler, '-I' + output_dir] + flags +
+                                       [file_paths, "-o", out, '-Wno-deprecated-gpu-targets'], stderr=subprocess.STDOUT)
+        print(res)
 
     def run(self, timeit=False):
         if timeit:
@@ -47,6 +49,7 @@ class NVCCPluginV2(Magics):
     @magic_arguments()
     @argument('-n', '--name', type=str, help='file name that will be produced by the cell. must end with .cu extension')
     @argument('-c', '--compile', type=bool, help='Should be compiled?')
+    @argument('-t', '--timeit', type=bool, help='Measure time?')
     @cell_magic
     def cuda(self, line='', cell=None):
         args = parse_argstring(self.cuda, line)
@@ -79,23 +82,34 @@ class NVCCPluginV2(Magics):
 
         return output
 
+    @magic_arguments()
+    @argument('-f', '--flag', type=str, help='flag for nvcc compiler')
+    @argument('-t', '--timeit', type=bool, help='Measure time?')
     @cell_magic
-    def cuda_run(self, line='', cell=None):
-        try:
-            args = self.argparser.parse_args(line.split())
-        except SystemExit:
-            self.argparser.print_help()
-            return
+    def cuf(self, line='', cell=None):
+        args = parse_argstring(self.cuf, line)
 
-        try:
-            cuda_src = os.listdir(self.output_dir)
-            cuda_src = [os.path.join(self.output_dir, x)
-                        for x in cuda_src if x[-3:] == '.cu']
-            print(f'found sources: {cuda_src}')
-            self.compile(self.output_dir, ' '.join(cuda_src), self.out)
-            output = self.run(timeit=args.timeit)
-        except subprocess.CalledProcessError as e:
-            helper.print_out(e.output.decode("utf8"))
-            output = None
+        if not os.path.exists(self.output_dir):
+            print(f'Output directory does not exist, creating')
+            try:
+                os.mkdir(self.output_dir)
+            except OSError:
+                print(f"Creation of the directory {self.output_dir} failed")
+            else:
+                print(f"Successfully created the directory {self.output_dir}")
+
+        flags=args.flag.split("+")
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            file_path = os.path.join(tmp_dir, str(uuid.uuid4()) + '.cu')
+            with open(file_path, "w") as f:
+                f.write(cell)
+
+            try:
+                self.compile(self.output_dir, file_path, self.out, flags)
+                output = self.run(timeit=args.timeit)
+            except subprocess.CalledProcessError as e:
+                helper.print_out(e.output.decode("utf8"))
+                output = None
 
         return output
